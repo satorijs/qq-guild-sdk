233256037cb226715bfb27fa22c6c1cc2549cdeb
yijie4188 <yijie4188@gmail.com>
2021-11-30 21:05:16 +0800

    chore(core): unit test get guilds and target id guild

--- packages/core/test/index.spec.ts
+++ packages/core/test/index.spec.ts
@@ -1,13 +1,29 @@
 import { Bot } from '@qq-guild-sdk/core'
+import { expect } from 'chai'
+
+declare namespace process {
+  let env: {
+    id: string
+    key: string
+    token: string
+  }
+}
 
 describe('Bot', () => {
-  it('should be able to create a bot', async () => {
-    const bot = new Bot({
-      app: {
-        id: '', key: '', token: ''
-      }
-    })
-    await bot.send.channel('', '')
-    await bot.send.channel.reply('', '', '')
+  const bot = new Bot({
+    app: {
+      id: process.env.id,
+      key: process.env.key,
+      token: process.env.token
+    }
+  })
+
+  it('should get guilds and guild for target id.', async () => {
+    const guilds = await bot.guilds
+    expect(guilds).to.be.an('array')
+    if (guilds.length > 0) {
+      expect(guilds[0]).to.be.an('object')
+      expect(guilds[0].name).to.be.eq(await bot.guild(guilds[0].id).then(g => g.name))
+    }
   })
 })
