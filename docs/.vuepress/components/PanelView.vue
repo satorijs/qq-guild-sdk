9c795368cef70c0f5a8dadea2dffe365d1eae650
yijie4188 <yijie4188@gmail.com>
2021-12-04 15:13:27 +0800

    fix(docs): pannel view

---
+++ docs/.vuepress/components/PanelView.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="panel-view">
+    <div class="title">123</div>
+    <template v-for="name of tabs">
+      <slot v-if="tab === name" :name="name"/>
+    </template>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { ref, computed, useSlots, onMounted } from 'vue'
+
+const
+  slots = useSlots(),
+  tab = ref('default'),
+  tabs = computed(() => Object.keys(slots))
+
+onMounted(() => {
+  console.log(Object.keys(slots))
+})
+</script>
+
+<style lang="scss">
+div.panel-view {
+}
+</style>
