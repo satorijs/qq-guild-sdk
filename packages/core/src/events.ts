c2bab287fef6408c9c9f97be3986e515e20c8443
yijie4188 <yijie4188@gmail.com>
2021-12-16 14:15:07 +0800

    feat(core): more event map

--- packages/core/src/events.ts
+++ packages/core/src/events.ts
@@ -1,5 +1,5 @@
 import { Message } from './sender'
-import { MessageReaction } from './common'
+import { Channel, Guild, MemberWithGuild, MessageReaction } from './common'
 
 export class Events {
   private readonly listeners: {
@@ -34,11 +34,29 @@ export class Events {
 export namespace Events {
   type Awaitable<T> = [T] extends [Promise<unknown>] ? T : T | Promise<T>
 
-  export interface Map {
+  interface GuildEventMap {
+    'guild:add': (guild: Guild) => Awaitable<void>
+    'guild:upd': (guild: Guild) => Awaitable<void>
+    'guild:del': (guild: Guild) => Awaitable<void>
+  }
+
+  interface ChannelEventMap {
+    'channel:add': (channel: Channel) => Awaitable<void>
+    'channel:upd': (channel: Channel) => Awaitable<void>
+    'channel:del': (channel: Channel) => Awaitable<void>
+  }
+
+  interface GuildMemberEventMap {
+    'guild-member:add': (mwg: MemberWithGuild) => Awaitable<void>
+    'guild-member:upd': (mwg: MemberWithGuild) => Awaitable<void>
+    'guild-member:del': (mwg: MemberWithGuild) => Awaitable<void>
+  }
+
+  export interface Map extends GuildEventMap, GuildMemberEventMap, ChannelEventMap {
     'ready': () => Awaitable<void>
     'error': (error: Error) => Awaitable<void>
     'message': (msg: Message) => Awaitable<string | void>
-    'reaction:add': (msg: MessageReaction) => Awaitable<void>
-    'reaction:del': (msg: MessageReaction) => Awaitable<void>
+    'reaction:add': (reaction: MessageReaction) => Awaitable<void>
+    'reaction:del': (reaction: MessageReaction) => Awaitable<void>
   }
 }
