190800461584aa08b14892f5540745efcc167842
yijie4188 <yijie4188@gmail.com>
2022-12-10 19:50:20 +0800

    feat(test): unit test reconnect logic

--- test/index.ts
+++ test/index.ts
@@ -1,5 +1,4 @@
 import { Bot } from '@qq-guild-sdk/core'
-import { AxiosError } from 'axios'
 
 const bot = new Bot({
   sandbox: true,
@@ -8,43 +7,24 @@ const bot = new Bot({
 
 async function main() {
   await bot.startClient(Bot.Intents.GUILD_MESSAGES | Bot.Intents.GUILDS)
-  await new Promise<void>(resolve => {
-    bot.on('ready', resolve)
+  await new Promise<void>(resolve => bot.on('ready', resolve))
+  console.log('qq guild bot is ready.')
+  bot.on('message', async m => {
+    console.log(`[${m.author.username}]: ${m.content}`)
+    if (m.content === 'ping') {
+      await bot.send.reply(m.id, {
+        type: m.channelId ? 'channel' : 'private',
+        id: m.channelId
+          ? m.channelId
+          : m.channelId
+      }, 'pong')
+    }
   })
-  await new Promise<void>((resolve, reject) => {
-    bot.on('message', async m => {
-      try {
-        await bot.send.channel.reply(m.id, m.channelId, {
-          messageReference: m.id,
-          content: 'Hello, world!',
-          fileImage: require('path').join(__dirname, 'test.png')
-        })
-        await bot.send.channel.reply(m.id, m.channelId, 'Hello, world!')
-        const filepath = require('path').join(__dirname, 'test.png')
-        const readStream = require('fs').createReadStream(filepath)
-        const buffer = await new Promise<Buffer>((resolve, reject) => {
-          const chunks: Buffer[] = []
-          readStream.on('data', (chunk: Buffer) => chunks.push(chunk))
-          readStream.on('end', () => resolve(Buffer.concat(chunks)))
-          readStream.on('error', reject)
-        })
-        await bot.send.channel.reply(m.id, m.channelId, {
-          fileImage: buffer
-        })
-        await bot.send.channel.reply(m.id, m.channelId, {
-          fileImage: require('fs').createReadStream(filepath)
-        })
-        await bot.send.channel.reply(m.id, m.channelId, {
-          fileImage: filepath
-        })
-        resolve()
-      } catch (e) {
-        if (e instanceof AxiosError)
-          console.error(e.request)
-        reject(e)
-      }
-    })
-  })
+  // test reconnect logic
+  // setTimeout(() => {
+  //   // @ts-ignore
+  //   bot.reconnectFuture.res?.()
+  // }, 2000)
 }
 
 main()
