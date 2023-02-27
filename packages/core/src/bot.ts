ad43527ad877ccc31b69d4f9fde84c72c45167fb
YiJie <51358815+NWYLZW@users.noreply.github.com>
2023-02-27 09:41:26 +0800

    Update packages/core/src/bot.ts

--- packages/core/src/bot.ts
+++ packages/core/src/bot.ts
@@ -114,7 +114,7 @@ export class Bot extends Api {
               payload.d.member.joinedAt = new Date(payload.d.member.joinedAt)
               this.emit('message', payload.d)
               break
-            case "DIRECT_MESSAGE_CREATE":
+            case 'DIRECT_MESSAGE_CREATE':
               payload.d.timestamp = new Date(payload.d.timestamp);
               payload.d.editedTimestamp = new Date(payload.d.editedTimestamp);
               payload.d.member.joinedAt = new Date(payload.d.member.joinedAt);
